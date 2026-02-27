77d91f5880662933b3091e128f7922958389bc80
AravitiReshmitha29 <vtu26450@veltech.edu.in>
2026-02-27 16:11:36 +0530

    Add files via upload

---
+++ week 4/demo-4.4/demo-1/src/main/java/com/example/component/CourseComponent.java
@@ -0,0 +1,11 @@
+package com.example.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseComponent {
+
+    public String getCourse() {
+        return "Course: Java";
+    }
+}
